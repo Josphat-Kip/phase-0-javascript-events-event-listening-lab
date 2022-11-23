8dcb6d4b3773e80fe80126395a72d54681f39248
Josphat-Kip <josphatkiplimo247@gmail.com>
2022-11-23 21:41:20 +0300

    Jose

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function addingEventListener() {
-  let input = document.getElementById("input");
-  input.addEventListener("click", () => {
-    console.log(addingEventListener);
-  });
-}
-addingEventListener("input");
+  const input = document.getElementById('input');
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  input.addEventListener('click', clickAlert);
+  }
+  addingEventListener();
